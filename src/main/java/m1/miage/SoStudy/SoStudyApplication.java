ec075556ea5a6d91a872a5b896f9631a17b7ac97
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2025-05-12 10:25:30 +0200

    initialisation du répository et de la forge logicielle

---
+++ src/main/java/m1/miage/SoStudy/SoStudyApplication.java
@@ -0,0 +1,13 @@
+package m1.miage.SoStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m1/miage/SoStudy/SoStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package m1.miage.SoStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
