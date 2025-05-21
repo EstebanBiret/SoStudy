ca70b90534935381a3ad246db0305fea82817c72
Dias Flavien <115453321+FlavienDIAS@users.noreply.github.com>
2025-05-21 10:48:30 +0200

    Update EventRepository.java

--- src/main/java/utc/miage/sostudy/repository/EventRepository.java
+++ src/main/java/utc/miage/sostudy/repository/EventRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import utc.miage.sostudy.model.entity.User;
 
 /**
  * Repository interface for Event entity.
@@ -37,4 +38,20 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
      * @return true if the user is a member of the event, false otherwise
      */
     boolean existsByUsers_IdUserAndEventId(Integer userId, Integer eventId);
+
+    /**
+     * Find all events by user
+     * @param user the user who created the event
+     * @return all events created by the user
+     */
+    @Query("SELECT e FROM Event e WHERE e.userCreator = :user")
+    List<Event> findByUserCreator(@Param("user")User user);
+
+    /**
+     * Find all events where the given user is a participant
+     * @param user the user who is a participant of the event
+     * @return all events the user is a participant of
+     */
+    @Query("SELECT e FROM Event e WHERE :user MEMBER OF e.users")
+    List<Event> findByUserParticipant(@Param("user") User user);
 }
